4e614053044f8f00738eb36ebd15cebb67db0959
sutee9 <stef@sutee.ch>
2026-02-11 16:19:02 +0100

    Updated Volumetric Fog

--- SuSc PlayVirtualSpaces/Assets/2_ThirdPartyAssets/Unity-URP-Volumetric-Light-ChristianQiu/Runtime/VolumetricFogRenderPassEvent.cs
+++
@@ -1,9 +0,0 @@
-/// <summary>
-/// Render pass events for the volumetric fog. Matches the ones in the Universal Render Pipeline.
-/// </summary>
-public enum VolumetricFogRenderPassEvent
-{
-	BeforeRenderingTransparents = 450,
-	AfterRenderingTransparents = 500,
-	BeforeRenderingPostProcessing = 550
-}
